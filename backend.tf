0c02dc6734ed708f88f18a8f5707e695a2e53490
André Frederico Lucas da Silva <andre.frederico@ifpr.edu.br>
2021-03-08 23:03:52 -0300

    Change bucket name

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@
 ### BACKEND
 terraform {
   backend "s3" {
-    bucket  = "terraform-state-files"
+    bucket  = "terraform-state-files-fredsilvas"
     key     = "core/vpc/vpc_prod/terraform-vpc-prod.tfstate"
     region  = "sa-east-1"
     encrypt = "true"
