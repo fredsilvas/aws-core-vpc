3414d2d715dd4f8562d96664b821130cd36f5267
André Frederico Lucas da Silva <andre.frederico@ifpr.edu.br>
2021-03-08 23:52:06 -0300

    Change provider file

--- provider.tf
+++ provider.tf
@@ -1,7 +1,17 @@
 ##############################################################################################
-### AWS PROVIDER
-provider "hashicorp/aws" {
-  version = "~> 2.0"
-  region  = var.aws_region
+### PROVIDER
+terraform {
+  required_version = ">= 0.13"
 
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.31.0"
+    }
+  }
+}
+
+
+provider "aws" {
+  region  = var.aws_region
 }
