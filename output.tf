0ecbda43b9d270ab1022098e3b9765fda6a38526
fredsilvas@gmail.com <fredsilvas@gmail.com>
2021-05-14 21:59:59 -0300

    Change name of output items

--- output.tf
+++ output.tf
@@ -28,70 +28,70 @@ output "az1" {
   value = var.az.0
 }
 
-## prod_public_subnet_az1
-output "prod_public_subnet_az1_id" {
-  value = aws_subnet.prod_public_subnet_az1.id
+## terraform_public_subnet_az1
+output "terraform_public_subnet_az1_id" {
+  value = aws_subnet.terraform_public_subnet_az1.id
 }
 
-output "prod_public_subnet_az1_cidr_ipv4" {
-  value = aws_subnet.prod_public_subnet_az1.cidr_block
+output "terraform_public_subnet_az1_cidr_ipv4" {
+  value = aws_subnet.terraform_public_subnet_az1.cidr_block
 }
 
-output "prod_public_subnet_az1_cidr_ipv6" {
-  value = aws_subnet.prod_public_subnet_az1.ipv6_cidr_block
+output "terraform_public_subnet_az1_cidr_ipv6" {
+  value = aws_subnet.terraform_public_subnet_az1.ipv6_cidr_block
 }
 
-## prod_private_web_subnet_az1
-output "prod_private_web_subnet_az1_id" {
-  value = aws_subnet.prod_private_web_subnet_az1.id
+## terraform_private_web_subnet_az1
+output "terraform_private_web_subnet_az1_id" {
+  value = aws_subnet.terraform_private_web_subnet_az1.id
 }
 
-output "prod_private_web_subnet_az1_cidr_ipv4" {
-  value = aws_subnet.prod_private_web_subnet_az1.cidr_block
+output "terraform_private_web_subnet_az1_cidr_ipv4" {
+  value = aws_subnet.terraform_private_web_subnet_az1.cidr_block
 }
 
-output "prod_private_web_subnet_az1_cidr_ipv6" {
-  value = aws_subnet.prod_private_web_subnet_az1.ipv6_cidr_block
+output "terraform_private_web_subnet_az1_cidr_ipv6" {
+  value = aws_subnet.terraform_private_web_subnet_az1.ipv6_cidr_block
 }
 
-/* output "prod_private_web_subnet_az1_net_ipv4" {
-  value = aws_nat_gateway.nat_gtw_prod_private_web_subnet_az1.public_ip
+/* output "terraform_private_web_subnet_az1_net_ipv4" {
+  value = aws_nat_gateway.nat_gtw_terraform_private_web_subnet_az1.public_ip
 } */
 
 
-## prod_private_app_subnet_az1
-output "prod_private_app_subnet_az1_id" {
-  value = aws_subnet.prod_private_app_subnet_az1.id
+## terraform_private_app_subnet_az1
+output "terraform_private_app_subnet_az1_id" {
+  value = aws_subnet.terraform_private_app_subnet_az1.id
 }
 
-output "prod_private_app_subnet_az1_cidr_ipv4" {
-  value = aws_subnet.prod_private_app_subnet_az1.cidr_block
+output "terraform_private_app_subnet_az1_cidr_ipv4" {
+  value = aws_subnet.terraform_private_app_subnet_az1.cidr_block
 }
 
-output "prod_private_app_subnet_az1_cidr_ipv6" {
-  value = aws_subnet.prod_private_app_subnet_az1.ipv6_cidr_block
+output "terraform_private_app_subnet_az1_cidr_ipv6" {
+  value = aws_subnet.terraform_private_app_subnet_az1.ipv6_cidr_block
 }
 
-/* output "prod_private_app_subnet_az1_net_ipv4" {
-  value = aws_nat_gateway.nat_gtw_prod_private_app_subnet_az1.public_ip
+/* output "terraform_private_app_subnet_az1_net_ipv4" {
+  value = aws_nat_gateway.nat_gtw_terraform_private_app_subnet_az1.public_ip
 } */
 
 
-## prod_private_data_subnet_az1
-output "prod_private_data_subnet_az1_id" {
-  value = aws_subnet.prod_private_data_subnet_az1.id
+## terraform_private_data_subnet_az1
+output "terraform_private_data_subnet_az1_id" {
+  value = aws_subnet.terraform_private_data_subnet_az1.id
 }
 
-output "prod_private_data_subnet_az1_cidr_ipv4" {
-  value = aws_subnet.prod_private_data_subnet_az1.cidr_block
+output "terraform_private_data_subnet_az1_cidr_ipv4" {
+  value = aws_subnet.terraform_private_data_subnet_az1.cidr_block
 }
 
-output "prod_private_data_subnet_az1_cidr_ipv6" {
-  value = aws_subnet.prod_private_data_subnet_az1.ipv6_cidr_block
+output "terraform_private_data_subnet_az1_cidr_ipv6" {
+  value = aws_subnet.terraform_private_data_subnet_az1.ipv6_cidr_block
 }
 
-/* output "prod_private_data_subnet_az1_net_ipv4" {
-  value = aws_nat_gateway.nat_gtw_prod_private_data_subnet_az1.public_ip
+/* output "terraform_private_data_subnet_az1_net_ipv4" {
+  value = aws_nat_gateway.nat_gtw_terraform_private_data_subnet_az1.public_ip
 } */
 
 
@@ -102,70 +102,70 @@ output "az2" {
   value = var.az.1
 }
 
-## prod_public_subnet_az2
-output "prod_public_subnet_az2_id" {
-  value = aws_subnet.prod_public_subnet_az2.id
+## terraform_public_subnet_az2
+output "terraform_public_subnet_az2_id" {
+  value = aws_subnet.terraform_public_subnet_az2.id
 }
 
-output "prod_public_subnet_az2_cidr_ipv4" {
-  value = aws_subnet.prod_public_subnet_az2.cidr_block
+output "terraform_public_subnet_az2_cidr_ipv4" {
+  value = aws_subnet.terraform_public_subnet_az2.cidr_block
 }
 
-output "prod_public_subnet_az2_cidr_ipv6" {
-  value = aws_subnet.prod_public_subnet_az2.ipv6_cidr_block
+output "terraform_public_subnet_az2_cidr_ipv6" {
+  value = aws_subnet.terraform_public_subnet_az2.ipv6_cidr_block
 }
 
-## prod_private_web_subnet_az2
-output "prod_private_web_subnet_az2_id" {
-  value = aws_subnet.prod_private_web_subnet_az2.id
+## terraform_private_web_subnet_az2
+output "terraform_private_web_subnet_az2_id" {
+  value = aws_subnet.terraform_private_web_subnet_az2.id
 }
 
-output "prod_private_web_subnet_az2_cidr_ipv4" {
-  value = aws_subnet.prod_private_web_subnet_az2.cidr_block
+output "terraform_private_web_subnet_az2_cidr_ipv4" {
+  value = aws_subnet.terraform_private_web_subnet_az2.cidr_block
 }
 
-output "prod_private_web_subnet_az2_cidr_ipv6" {
-  value = aws_subnet.prod_private_web_subnet_az2.ipv6_cidr_block
+output "terraform_private_web_subnet_az2_cidr_ipv6" {
+  value = aws_subnet.terraform_private_web_subnet_az2.ipv6_cidr_block
 }
 
-/* output "prod_private_web_subnet_az2_net_ipv4" {
-  value = aws_nat_gateway.nat_gtw_prod_private_web_subnet_az2.public_ip
+/* output "terraform_private_web_subnet_az2_net_ipv4" {
+  value = aws_nat_gateway.nat_gtw_terraform_private_web_subnet_az2.public_ip
 } */
 
 
-## prod_private_app_subnet_az2
-output "prod_private_app_subnet_az2_id" {
-  value = aws_subnet.prod_private_app_subnet_az2.id
+## terraform_private_app_subnet_az2
+output "terraform_private_app_subnet_az2_id" {
+  value = aws_subnet.terraform_private_app_subnet_az2.id
 }
 
-output "prod_private_app_subnet_az2_cidr_ipv4" {
-  value = aws_subnet.prod_private_app_subnet_az2.cidr_block
+output "terraform_private_app_subnet_az2_cidr_ipv4" {
+  value = aws_subnet.terraform_private_app_subnet_az2.cidr_block
 }
 
-output "prod_private_app_subnet_az2_cidr_ipv6" {
-  value = aws_subnet.prod_private_app_subnet_az2.ipv6_cidr_block
+output "terraform_private_app_subnet_az2_cidr_ipv6" {
+  value = aws_subnet.terraform_private_app_subnet_az2.ipv6_cidr_block
 }
 
-/* output "prod_private_app_subnet_az2_net_ipv4" {
-  value = aws_nat_gateway.nat_gtw_prod_private_app_subnet_az2.public_ip
+/* output "terraform_private_app_subnet_az2_net_ipv4" {
+  value = aws_nat_gateway.nat_gtw_terraform_private_app_subnet_az2.public_ip
 } */
 
 
-## prod_private_data_subnet_az2
-output "prod_private_data_subnet_az2_id" {
-  value = aws_subnet.prod_private_data_subnet_az2.id
+## terraform_private_data_subnet_az2
+output "terraform_private_data_subnet_az2_id" {
+  value = aws_subnet.terraform_private_data_subnet_az2.id
 }
 
-output "prod_private_data_subnet_az2_cidr_ipv4" {
-  value = aws_subnet.prod_private_data_subnet_az2.cidr_block
+output "terraform_private_data_subnet_az2_cidr_ipv4" {
+  value = aws_subnet.terraform_private_data_subnet_az2.cidr_block
 }
 
-output "prod_private_data_subnet_az2_cidr_ipv6" {
-  value = aws_subnet.prod_private_data_subnet_az2.ipv6_cidr_block
+output "terraform_private_data_subnet_az2_cidr_ipv6" {
+  value = aws_subnet.terraform_private_data_subnet_az2.ipv6_cidr_block
 }
 
-/* output "prod_private_data_subnet_az2_net_ipv4" {
-  value = aws_nat_gateway.nat_gtw_prod_private_data_subnet_az2.public_ip
+/* output "terraform_private_data_subnet_az2_net_ipv4" {
+  value = aws_nat_gateway.nat_gtw_terraform_private_data_subnet_az2.public_ip
 } */
 
 
@@ -176,69 +176,69 @@ output "az3" {
   value = var.az.2
 }
 
-## prod_public_subnet_az3
-output "prod_public_subnet_az3_id" {
-  value = aws_subnet.prod_public_subnet_az3.id
+## terraform_public_subnet_az3
+output "terraform_public_subnet_az3_id" {
+  value = aws_subnet.terraform_public_subnet_az3.id
 }
 
-output "prod_public_subnet_az3_cidr_ipv4" {
-  value = aws_subnet.prod_public_subnet_az3.cidr_block
+output "terraform_public_subnet_az3_cidr_ipv4" {
+  value = aws_subnet.terraform_public_subnet_az3.cidr_block
 }
 
-output "prod_public_subnet_az3_cidr_ipv6" {
-  value = aws_subnet.prod_public_subnet_az3.ipv6_cidr_block
+output "terraform_public_subnet_az3_cidr_ipv6" {
+  value = aws_subnet.terraform_public_subnet_az3.ipv6_cidr_block
 }
 
 
-## prod_private_web_subnet_az3
-output "prod_private_web_subnet_az3_id" {
-  value = aws_subnet.prod_private_web_subnet_az3.id
+## terraform_private_web_subnet_az3
+output "terraform_private_web_subnet_az3_id" {
+  value = aws_subnet.terraform_private_web_subnet_az3.id
 }
 
-output "prod_private_web_subnet_az3_cidr_ipv4" {
-  value = aws_subnet.prod_private_web_subnet_az3.cidr_block
+output "terraform_private_web_subnet_az3_cidr_ipv4" {
+  value = aws_subnet.terraform_private_web_subnet_az3.cidr_block
 }
 
-output "prod_private_web_subnet_az3_cidr_ipv6" {
-  value = aws_subnet.prod_private_web_subnet_az3.ipv6_cidr_block
+output "terraform_private_web_subnet_az3_cidr_ipv6" {
+  value = aws_subnet.terraform_private_web_subnet_az3.ipv6_cidr_block
 }
 
-/* output "prod_private_web_subnet_az3_net_ipv4" {
-  value = aws_nat_gateway.nat_gtw_prod_private_web_subnet_az3.public_ip
+/* output "terraform_private_web_subnet_az3_net_ipv4" {
+  value = aws_nat_gateway.nat_gtw_terraform_private_web_subnet_az3.public_ip
 } */
 
 
-## prod_private_app_subnet_az3
-output "prod_private_app_subnet_az3_id" {
-  value = aws_subnet.prod_private_app_subnet_az3.id
+## terraform_private_app_subnet_az3
+output "terraform_private_app_subnet_az3_id" {
+  value = aws_subnet.terraform_private_app_subnet_az3.id
 }
 
-output "prod_private_app_subnet_az3_cidr_ipv4" {
-  value = aws_subnet.prod_private_app_subnet_az3.cidr_block
+output "terraform_private_app_subnet_az3_cidr_ipv4" {
+  value = aws_subnet.terraform_private_app_subnet_az3.cidr_block
 }
 
-output "prod_private_app_subnet_az3_cidr_ipv6" {
-  value = aws_subnet.prod_private_app_subnet_az3.ipv6_cidr_block
+output "terraform_private_app_subnet_az3_cidr_ipv6" {
+  value = aws_subnet.terraform_private_app_subnet_az3.ipv6_cidr_block
 }
 
-/* output "prod_private_app_subnet_az3_net_ipv4" {
-  value = aws_nat_gateway.nat_gtw_prod_private_app_subnet_az3.public_ip
+/* output "terraform_private_app_subnet_az3_net_ipv4" {
+  value = aws_nat_gateway.nat_gtw_terraform_private_app_subnet_az3.public_ip
 } */
 
 
-## prod_private_data_subnet_az3
-output "prod_private_data_subnet_az3_id" {
-  value = aws_subnet.prod_private_data_subnet_az3.id
+## terraform_private_data_subnet_az3
+output "terraform_private_data_subnet_az3_id" {
+  value = aws_subnet.terraform_private_data_subnet_az3.id
 }
 
-output "prod_private_data_subnet_az3_cidr_ipv4" {
-  value = aws_subnet.prod_private_data_subnet_az3.cidr_block
+output "terraform_private_data_subnet_az3_cidr_ipv4" {
+  value = aws_subnet.terraform_private_data_subnet_az3.cidr_block
 }
 
-output "prod_private_data_subnet_az3_cidr_ipv6" {
-  value = aws_subnet.prod_private_data_subnet_az3.ipv6_cidr_block
+output "terraform_private_data_subnet_az3_cidr_ipv6" {
+  value = aws_subnet.terraform_private_data_subnet_az3.ipv6_cidr_block
 }
 
-/* output "prod_private_data_subnet_az3_net_ipv4" {
-  value = aws_nat_gateway.nat_gtw_prod_private_data_subnet_az3.public_ip
+/* output "terraform_private_data_subnet_az3_net_ipv4" {
+  value = aws_nat_gateway.nat_gtw_terraform_private_data_subnet_az3.public_ip
 } */
